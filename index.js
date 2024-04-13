4603365a2a392375897a00cf22446e515a15e6f5
M-b-o-n-g-e-n-i <hopelezinto317@gmail.com>
2024-04-13 11:13:35 +0200

    it's chaai

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //create cars api using express
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+const PORT = process.env.PORT || 3001;
 
 const cars = require('./cars.json');
 
@@ -47,6 +50,6 @@ app.post('/cars', (req, res) => {
 });
 
 //start app at localhost:3001
-app.listen(3001, () => {
-    console.log('Server started at http://localhost:3001');
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
 });
